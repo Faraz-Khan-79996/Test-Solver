ceb6a157a50ba513466c88be20d9ba5c0aae5d7b
Faraz-Khan-79996 <farazgod1234@gmail.com>
2024-08-20 17:58:13 +0530

    Port mapping added for render

--- server/index.js
+++ server/index.js
@@ -73,6 +73,8 @@ app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
